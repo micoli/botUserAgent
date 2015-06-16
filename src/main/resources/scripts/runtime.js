4f0567940921108c0da5fb14bbd56b1742a6c7fa
o.michaud <olivier@micoli.org>
2015-06-16 02:00:33 +0200

    multiple peers

--- src/main/resources/scripts/runtime.js
+++ src/main/resources/scripts/runtime.js
@@ -1,30 +1,66 @@
 load('src/main/resources/scripts/underscore-min.js');
 load('src/main/resources/scripts/backbone-min.js');
 
+
 var Platform = Java.type("javafx.application.Platform");
 var Timer    = Java.type("java.util.Timer");
 
-function setInterval(func, milliseconds) {
-	// New timer, run as daemon so the application can quit
-	var timer = new Timer("setInterval", true);
-	timer.schedule(function() Platform.runLater(func), milliseconds, milliseconds);
+function setTimerRequest(handler, delay, interval, args) {
+	handler = handler || function() {};
+	delay = delay || 0;
+	interval = interval || 0;
+
+	var applyHandler = function() handler.apply(this, args);
+	var runLater = function() Platform.runLater(applyHandler);
+
+	var timer = new Timer("setTimerRequest", true);
+
+	if (interval > 0) {
+		timer.schedule(runLater, delay, interval);
+	} else {
+		timer.schedule(runLater, delay);
+	}
+
 	return timer;
+}
+
+function clearTimerRequest(timer) {
+	timer.cancel();
+}
+
+function setInterval() {
+	var args = Array.prototype.slice.call(arguments);
+	var handler = args.shift();
+	var ms = args.shift();
+
+	return setTimerRequest(handler, ms, ms, args);
 }
 
 function clearInterval(timer) {
-	timer.cancel();
+	clearTimerRequest(timer);
 }
 
-function setTimeout(func, milliseconds) {
-	// New timer, run as daemon so the application can quit
-	var timer = new Timer("setTimeout", true);
-	timer.schedule(function() Platform.runLater(func), milliseconds);
-	return timer;
+function setTimeout() {
+	var args = Array.prototype.slice.call(arguments);
+	var handler = args.shift();
+	var ms = args.shift();
+
+	return setTimerRequest(handler, ms, 0, args);
 }
 
 function clearTimeout(timer) {
-	timer.cancel();
+	clearTimerRequest(timer);
 }
 
+function setImmediate() {
+	var args = Array.prototype.slice.call(arguments);
+	var handler = args.shift();
+
+	return setTimerRequest(handler, 0, 0, args);
+}
+
+function clearImmediate(timer) {
+	clearTimerRequest(timer);
+}
 
 print("loaded");
