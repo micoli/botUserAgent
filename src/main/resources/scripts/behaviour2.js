85af40a82d885c2af43f3fae7be92be7f52af354
o.michaud <olivier@micoli.org>
2015-06-17 01:09:03 +0200

    better serialization for sipMessages

--- src/main/resources/scripts/behaviour2.js
+++
@@ -1,5 +0,0 @@
-this.behaviour2 = this.behaviour.extend({
-	registerSuccessful : function(args /*sipResponse,config*/){
-		print(this.id+" :: registerSuccessful",args[1].getUserPart()+" behaviour2");
-	},
-});
