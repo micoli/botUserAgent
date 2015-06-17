703015d7ea6ba481ed91af9195e6368a424c1804
Olivier MICHAUD <olivier@micoli.org>
2015-06-17 08:59:39 +0200

    mini repl shell

--- src/main/resources/scripts/behaviour2.js
+++ src/main/resources/scripts/behaviour2.js
@@ -1,5 +1,20 @@
 this.behaviour2 = this.behaviour.extend({
 	registerSuccessful : function(sipResponse,config){
 		print(this.id+" :: registerSuccessful",config.getUserPart()+" behaviour2");
-	}
+	},
+	remoteHangup : function(sipRequest){
+		var that = this;
+		//LE LOURD
+		//sipRequest = JSON.parse(sipRequest);
+		print(this.id+" :: remoteHangup",sipRequest);
+		//var m = sipRequest.sipHeaders.From.match(/sip\:([0-9]*)/);
+		var m = sipRequest.match(/"from":"<sip:([0-9]*)\@(.*)"/);
+		// 201 call 101@10.80.0.95
+		if(m){
+			setTimeout(function(){
+				that.get('ua').call("sip:"+m[1]+"@10.80.0.95");
+			},500);
+		}
+	},
+
 });
