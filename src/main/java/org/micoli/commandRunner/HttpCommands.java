a31d69a6ec1a5b50bda936a29a389f9e281d1ca4
o.michaud <o.michaud@servicemagic.eu>
2015-10-19 13:54:18 +0200

    add a small http server

---
+++ src/main/java/org/micoli/commandRunner/HttpCommands.java
@@ -0,0 +1,20 @@
+package org.micoli.commandRunner;
+
+import java.io.IOException;
+
+
+public class HttpCommands extends GenericCommands {
+
+	public HttpCommands(CommandRunner commandRunner) {
+		super(commandRunner);
+	}
+
+	public void run() {
+		try {
+			HttpCommandsServer httpCommandsServer = new HttpCommandsServer();
+			httpCommandsServer.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/org/micoli/commandRunner/HttpCommandsServer.java
@@ -0,0 +1,36 @@
+package org.micoli.commandRunner;
+
+import java.io.IOException;
+import java.util.Map;
+
+import fi.iki.elonen.NanoHTTPD;
+
+
+
+public class HttpCommandsServer extends NanoHTTPD {
+
+	public HttpCommandsServer() throws IOException {
+		super(8081);
+	}
+
+	@Override
+	public Response serve(IHTTPSession session) {
+		Method method = session.getMethod();
+		String uri = session.getUri();
+		System.out.println(method + " '" + uri + "' ");
+
+		String msg = "<html><body><h1>Hello server</h1>\n";
+		Map<String, String> parms = session.getParms();
+		if (parms.get("username") == null){
+			msg +="<form action='?' method='get'>\n" +
+				"  <p>Your name: <input type='text' name='username'></p>\n" +
+				"</form>\n";
+		}else{
+			msg += "<p>Hello, " + parms.get("username") + "!</p>";
+		}
+
+		msg += "</body></html>\n";
+
+		return new NanoHTTPD.Response(msg);
+	}
+}
